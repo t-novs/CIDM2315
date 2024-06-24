6dd313a0a23fa548aa5ebc9e26cc4711d8e340bf
t-novs <tnovus1@buffs.wtamu.edu>
2024-06-24 18:20:53 +0200

    NumberPattern

---
+++ NumberPattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace NumberPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Assign an int value to N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 0; j < N - i; j++)
+                {
+                    Console.Write(" ");
+                }
+
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write(i);
+                }
+                
+                Console.WriteLine();
+            }
+        }
+    }
+}
