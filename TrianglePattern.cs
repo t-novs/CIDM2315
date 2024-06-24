06eb0e9e4608a2edd9317fb1632606672deaebb3
t-novs <tnovus1@buffs.wtamu.edu>
2024-06-24 18:19:51 +0200

    TrianglePattern

---
+++ TrianglePattern.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TrianglePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Assign an int value to N: ");
+            int N = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= N; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
