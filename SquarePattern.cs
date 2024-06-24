cc88c128aa753e9dc2a55d0bfe12d62418a708e9
t-novs <tnovus1@buffs.wtamu.edu>
2024-06-24 18:19:01 +0200

    SquarePattern

---
+++ SquarePattern.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquarePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Prompt the user to input an integer value for N
+            Console.Write("Assign an int value to N: ");
+            
+            // Read the input from the user and convert it to an integer
+            int N = int.Parse(Console.ReadLine());
+
+            // Loop to print N rows
+            for (int i = 0; i < N; i++)
+            {
+
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("#");
+                }
+
+                Console.WriteLine();
+            }
+        }
+    }
+}
