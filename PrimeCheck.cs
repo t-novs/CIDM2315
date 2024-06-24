be3c2b3682951b14db01f7b4ac1fe00f0373e248
t-novs <tnovus1@buffs.wtamu.edu>
2024-06-24 18:17:40 +0200

    PrimeCheck

---
+++ PrimeCheck.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace PrimeCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Input an integer: ");
+            int N = int.Parse(Console.ReadLine());
+            bool isPrime = true;
+
+            if (N <= 1)
+            {
+                isPrime = false;
+            }
+            else{
+                for (int i = 2; i <= Math.Sqrt(N); i++)
+                {
+                    if (N % i == 0)
+                    {
+                        isPrime = false;
+                        break;
+                    }
+                }
+            }
+
+            if (isPrime)
+            {
+                Console.WriteLine("N is prime");
+            }
+            else
+            {
+                Console.WriteLine("N is non-prime");
+            }
+        }
+    }
+}
