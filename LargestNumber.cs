ccaeedda5d192cfc8aab7eb05f9eee9c534d4876
t-novs <tnovus1@buffs.wtamu.edu>
2024-07-01 19:06:53 +0200

    LargestNumber

---
+++ LargestNumber.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace LargestNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first integer: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Enter the second integer: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int largest = GetLargest(a, b);
+            Console.WriteLine($"The largest number is: {largest}");
+        }
+
+        static int GetLargest(int num1, int num2)
+        {
+            return num1 > num2 ? num1 : num2;
+        }
+    }
+}
